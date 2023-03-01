253a55b01b74fb76935c1d48b3112be0eea47e45
Carson Sobolewski <47485699+csobolew@users.noreply.github.com>
2023-03-01 17:42:14 -0500

    functional cypress home test

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,16 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('http://localhost:4200/home')
+    cy.intercept({
+      method: 'POST',
+      url: 'http://localhost:9000/walmart',
+    }).as('apiCheck')
     cy.get('input').type('test')
+    cy.get('#share').click()
+    cy
+    .wait('@apiCheck')
+    .then(intercept => {
+     
+    });
   })
 })
