a8f38fdbf6ab818a0890250625f22fffe929fbff
Carson Sobolewski <47485699+csobolew@users.noreply.github.com>
2023-03-02 01:49:06 -0500

    Update home.cy.ts

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('http://127.0.0.1:4200/home')
+    cy.visit('http://localhost:4200/home')
     cy.intercept({
       method: 'POST',
       url: 'http://127.0.0.1:9000/walmart',
