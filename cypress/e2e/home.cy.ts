645cb38e934fd4fdf27ca1fb5d41920babc59559
Carson Sobolewski <47485699+csobolew@users.noreply.github.com>
2023-03-02 12:15:33 -0500

    fix cypress IP

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -3,7 +3,6 @@ describe('template spec', () => {
     cy.visit('http://localhost:4200/home')
     cy.intercept({
       method: 'POST',
-      url: 'http://127.0.0.1:9000/walmart',
     }).as('apiCheck')
     cy.get('input').type('test')
     cy.get('#share').click()
