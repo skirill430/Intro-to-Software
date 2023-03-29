1206ebbf1fa154b3e3bf946f0b3fdc33457dc5c6
Kirill Sokolov <sokolovk@ufl.edu>
2023-03-29 17:31:03 -0400

    walmart added to bothStores

--- server/handlers/stores.go
+++ server/handlers/stores.go
@@ -127,6 +127,9 @@ func BothStores(w http.ResponseWriter, r *http.Request) {
 	for i != -1 {
 		i = strings.Index(itemString, "parent")
 		if i == -1 {
+			i = strings.Index(itemString, "enrichment")
+		}
+		if i == -1 {
 			break
 		}
 		i2 = len(itemString) - 1
@@ -151,7 +154,7 @@ func BothStores(w http.ResponseWriter, r *http.Request) {
 
 		//fmt.Printf("made it this far4")
 
-		i = strings.Index(itemString, "title")
+		i = strings.Index(itemString, "\"title\"")
 		if i == -1 {
 			break
 		}
@@ -201,39 +204,120 @@ func BothStores(w http.ResponseWriter, r *http.Request) {
 		//fmt.Printf(NewJson)
 	}
 	NewJson = NewJson[0 : len(NewJson)-2]
+	NewJson = NewJson + "},"
+
+	search, err = io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//append = bytes.NewBuffer(search).String()
+
+	url = "https://walmart.p.rapidapi.com/products/v2/list?cat_id=0&sort=best_seller&page=1&query="
+
+	url = url + append
+	req, _ = http.NewRequest("GET", url, nil)
+
+	req.Header.Add("X-RapidAPI-Key", "813328aa2cmshbaf8f8dc041bb3ep1a7203jsnc647b6bcd1c7")
+	req.Header.Add("X-RapidAPI-Host", "walmart.p.rapidapi.com")
+
+	res, err = http.DefaultClient.Do(req)
+
+	if err != nil {
+		fmt.Println("An error connecting to Walmart occurred.")
+		return
+	}
+
+	defer res.Body.Close()
+	body, _ = io.ReadAll(res.Body)
+	i = 0
+	itemString = string(body)
+	i2 = len(itemString) - 1
+	i3 = 0
+	for i != -1 {
+		i = strings.Index(itemString, "\"Product\"")
+		if i == -1 {
+			break
+		}
+		i2 = len(itemString) - 1
+		itemString = itemString[i:i2]
+		//fmt.Printf("made it this far3")
+
+		i = strings.Index(itemString, "\"name\"")
+		if i == -1 {
+			break
+		}
+		//fmt.Printf("made it this far4")
+		i2 = len(itemString) - 1
+		fmt.Printf("i = %d, i2 = %d\n", i, i2)
+		itemString = itemString[i:i2]
+		//fmt.Printf("made it this far5")
+		i3 = strings.Index(itemString, ",")
+		//fmt.Printf("made it this far6")
+		fmt.Printf("i = %d, i3 = %d\n", i, i3)
+		NewJson = NewJson + "{\"name\":"
+		NewJson = NewJson + itemString[7:i3-1] //contains the name
+		NewJson = NewJson + "\","
+
+		//fmt.Printf("made it this far4")
+
+		i = strings.Index(itemString, "thumbnailUrl")
+		if i == -1 {
+			break
+		}
+		i2 = len(itemString) - 1
+		itemString = itemString[i:i2]
+		i3 = strings.Index(itemString, "}")
+		fmt.Printf("i = %d, i3 = %d\n", i, i3)
+		NewJson = NewJson + "\"imgUrl\":"
+		NewJson = NewJson + " \"" + itemString[15:i3-1] //contains the url
+		if strings.Contains(itemString[16:i3-1], "\"") {
+			NewJson = NewJson + ","
+		} else {
+			NewJson = NewJson + "\","
+		}
+
+		//fmt.Printf("made it this far5")
+
+		i = strings.Index(itemString, "averageRating")
+		if i == -1 {
+			break
+		}
+		i2 = len(itemString) - 1
+		itemString = itemString[i:i2]
+		i3 = strings.Index(itemString, ",")
+		fmt.Printf("i = %d, i3 = %d\n", i, i3)
+		NewJson = NewJson + "\"rating\":"
+		NewJson = NewJson + " \"" + itemString[15:i3-1] //contains the rating
+		NewJson = NewJson + "\","
+
+		//fmt.Printf("made it this far6")
+
+		i = strings.Index(itemString, "$")
+		if i == -1 {
+			break
+		}
+		i2 = len(itemString) - 1
+		itemString = itemString[i:i2]
+		i3 = strings.Index(itemString, ",")
+		fmt.Printf("i = %d, i3 = %d\n", i, i3)
+		NewJson = NewJson + "\"pricing\":"
+		NewJson = NewJson + "\"" + itemString[1:i3-1] //contains the price
+		if strings.Contains(itemString[1:i3-1], "\"") {
+			NewJson = NewJson + ","
+		} else {
+			NewJson = NewJson + "\","
+		}
+		NewJson = NewJson + "\"seller_name\":\"Walmart\"},"
+
+		//fmt.Printf("made it this far7")
+		//fmt.Printf(NewJson)
+	}
+	NewJson = NewJson[0 : len(NewJson)-2]
 	NewJson = NewJson + "}]"
 	fmt.Print(NewJson)
 
 	/*
-		search, err = io.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		append = bytes.NewBuffer(search).String()
-
-		url = "https://walmart.p.rapidapi.com/products/v2/list?cat_id=0&sort=best_seller&page=1&query="
-
-		url = url + append
-		req, _ = http.NewRequest("GET", url, nil)
-
-		req.Header.Add("X-RapidAPI-Key", "813328aa2cmshbaf8f8dc041bb3ep1a7203jsnc647b6bcd1c7")
-		req.Header.Add("X-RapidAPI-Host", "walmart.p.rapidapi.com")
-
-		res, err = http.DefaultClient.Do(req)
-
-		if err != nil {
-			fmt.Println("An error connecting to Walmart occurred.")
-			return
-		}
-
-		defer res.Body.Close()
-		body, _ = io.ReadAll(res.Body)
-
-		i = 0
-		itemString = string(body)
-		i2 = len(itemString) - 1
-		i3 = 0
 		for i != -1 {
 			i = strings.Index(itemString, "\"Product\"")
 			itemString = itemString[i:i2]
