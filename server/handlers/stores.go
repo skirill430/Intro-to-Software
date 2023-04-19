f921ca8fe50bf02a4fe5f57fb1fe1317f80cedd8
Kirill Sokolov <sokolovk@ufl.edu>
2023-04-19 14:52:36 -0400

    fixed empty response error

--- server/handlers/stores.go
+++ server/handlers/stores.go
@@ -120,251 +120,259 @@ func BothStores(w http.ResponseWriter, r *http.Request) {
 
 	NewJson := `[`
 	i := 0
+	temp1 := 0
+	temp2 := 0
 	itemString = string(body)
 	//fmt.Printf(itemString)
 	var i2 int
 	i3 := 0
 	count := 0
-	for i != -1 {
-		if count >= 20 {
-			break
-		}
-		i = strings.Index(itemString, "enrichment\":{\"buy_url")
-		if i == -1 {
-			break
-		}
-		i = strings.Index(itemString, "primary_image_url")
-		if i == -1 {
-			break
-		}
-		i = strings.Index(itemString, "\"title\"")
-		if i == -1 {
-			break
-		}
-		i = strings.Index(itemString, "formatted_current_price\"")
-		if i == -1 {
-			break
-		}
-		i = strings.Index(itemString, "average")
-		if i == -1 {
-			break
-		}
-
-		i = strings.Index(itemString, "enrichment\":{\"buy_url")
-
-		i2 = len(itemString) - 1
-		itemString = itemString[i:i2]
-		//fmt.Printf("made it this far3")
-
-		i = strings.Index(itemString, "primary_image_url")
-		//fmt.Printf("made it this far4")
-		i2 = len(itemString) - 1
-		//fmt.Printf("i = %d, i3 = %d\n", i, i2)
-		itemString = itemString[i:i2]
-		//fmt.Printf("made it this far5")
-		i3 = strings.Index(itemString, "}")
-		//fmt.Printf("made it this far6")
-		//fmt.Printf("i = %d, i3 = %d\n", i, i3)
-		NewJson = NewJson + "{\"image_url\":"
-		NewJson = NewJson + itemString[19:i3-1] //contains the url
-		NewJson = NewJson + "\","
-
-		//fmt.Printf("made it this far4")
-
-		i = strings.Index(itemString, "\"title\"")
-		i2 = len(itemString) - 1
-		itemString = itemString[i:i2]
-		i3 = strings.Index(itemString, "}")
-		//fmt.Printf("i = %d, i3 = %d\n", i, i3)
-		NewJson = NewJson + "\"product_name\""
-		NewJson = NewJson + itemString[7:i3] //contains the name
-		if strings.Contains(itemString[8:i3], "\"") {
-			NewJson = NewJson + ","
-		} else {
-			NewJson = NewJson + "\","
-		}
-
-		//fmt.Printf("made it this far5")
-
-		i = strings.Index(itemString, "formatted_current_price\"")
-		i2 = len(itemString) - 1
-		itemString = itemString[i:i2]
-		i3 = strings.Index(itemString, ",")
-		//fmt.Printf("i = %d, i3 = %d\n", i, i3)
-		NewJson = NewJson + "\"price\":"
-		NewJson = NewJson + "\"" + itemString[26:i3] //contains the price
-		if strings.Contains(itemString[27:i3], "\"") {
-			if NewJson[len(NewJson)-1:] != "\"" {
-				NewJson = NewJson + "\""
+	temp1 = strings.Index(itemString, "enrichment\":{\"buy_url")
+	temp2 = strings.Index(itemString, "\"Product\"")
+	if temp1 == -1 && temp2 == -1 {
+		NewJson = "{}"
+	} else {
+		for i != -1 {
+			if count >= 20 {
+				break
 			}
-			NewJson = NewJson + ","
-		} else {
-			NewJson = NewJson + "\","
-		}
-		//fmt.Printf("made it this far6")
+			i = strings.Index(itemString, "enrichment\":{\"buy_url")
+			if i == -1 {
+				break
+			}
+			i = strings.Index(itemString, "primary_image_url")
+			if i == -1 {
+				break
+			}
+			i = strings.Index(itemString, "\"title\"")
+			if i == -1 {
+				break
+			}
+			i = strings.Index(itemString, "formatted_current_price\"")
+			if i == -1 {
+				break
+			}
+			i = strings.Index(itemString, "average")
+			if i == -1 {
+				break
+			}
 
-		i = strings.Index(itemString, "average")
-		i2 = len(itemString) - 1
-		itemString = itemString[i:i2]
-		i3 = strings.Index(itemString, ",")
-		temp := itemString[9 : i3-1]
-		tempi := strings.Index(temp, "nul")
-		if tempi != -1 {
-			NewJson = NewJson + "\"rating\":"
-			NewJson = NewJson + "\"" + "" + "\"" //contains the rating
-			NewJson = NewJson + ","
-		} else {
+			i = strings.Index(itemString, "enrichment\":{\"buy_url")
+
+			i2 = len(itemString) - 1
+			itemString = itemString[i:i2]
+			//fmt.Printf("made it this far3")
+
+			i = strings.Index(itemString, "primary_image_url")
+			//fmt.Printf("made it this far4")
+			i2 = len(itemString) - 1
+			//fmt.Printf("i = %d, i3 = %d\n", i, i2)
+			itemString = itemString[i:i2]
+			//fmt.Printf("made it this far5")
+			i3 = strings.Index(itemString, "}")
+			//fmt.Printf("made it this far6")
 			//fmt.Printf("i = %d, i3 = %d\n", i, i3)
-			NewJson = NewJson + "\"rating\":"
-			NewJson = NewJson + "\"" + itemString[9:i3-1] + "\"" //contains the rating
-			NewJson = NewJson + ","
-		}
-		NewJson = NewJson + "\"seller_name\":\"Target\"},"
-
-		//fmt.Printf("made it this far7")
-		//fmt.Printf(NewJson)
-		count++
-	}
-	NewJson = NewJson[0 : len(NewJson)-2]
-	NewJson = NewJson + "},"
-
-	search, err = io.ReadAll(r.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	//append = bytes.NewBuffer(search).String()
-
-	url = "https://walmart.p.rapidapi.com/products/v2/list?cat_id=0&sort=best_seller&page=1&query="
-
-	url = url + append
-	req, _ = http.NewRequest("GET", url, nil)
-
-	req.Header.Add("X-RapidAPI-Key", "813328aa2cmshbaf8f8dc041bb3ep1a7203jsnc647b6bcd1c7")
-	req.Header.Add("X-RapidAPI-Host", "walmart.p.rapidapi.com")
-
-	res, err = http.DefaultClient.Do(req)
-
-	if err != nil {
-		fmt.Println("An error connecting to Walmart occurred.")
-		return
-	}
-
-	defer res.Body.Close()
-	body, _ = io.ReadAll(res.Body)
-	i = 0
-	itemString = string(body)
-	i2 = len(itemString) - 1
-	i3 = 0
-	count = 0
-	for i != -1 {
-		if count >= 20 {
-			break
-		}
-		i = strings.Index(itemString, "\"Product\"")
-		if i == -1 {
-			break
-		}
-		i = strings.Index(itemString, "\"name\"")
-		if i == -1 {
-			break
-		}
-		i = strings.Index(itemString, "thumbnailUrl")
-		if i == -1 {
-			break
-		}
-		i = strings.Index(itemString, "averageRating")
-		if i == -1 {
-			break
-		}
-		i = strings.Index(itemString, "$")
-		if i == -1 {
-			break
-		}
-
-		i = strings.Index(itemString, "\"Product\"")
-
-		i2 = len(itemString) - 1
-		itemString = itemString[i:i2]
-		//fmt.Printf("made it this far3")
-
-		i = strings.Index(itemString, "\"name\"")
-
-		//fmt.Printf("made it this far4")
-		i2 = len(itemString) - 1
-		//fmt.Printf("i = %d, i2 = %d\n", i, i2)
-		itemString = itemString[i:i2]
-		//fmt.Printf("made it this far5")
-		i3 = strings.Index(itemString, ",")
-		//fmt.Printf("made it this far6")
-		//fmt.Printf("i = %d, i3 = %d\n", i, i3)
-		NewJson = NewJson + "{\"product_name\":"
-		NewJson = NewJson + itemString[7:i3-1] //contains the name
-		NewJson = NewJson + "\","
-
-		//fmt.Printf("made it this far4")
-
-		i = strings.Index(itemString, "thumbnailUrl")
-
-		i2 = len(itemString) - 1
-		itemString = itemString[i:i2]
-		i3 = strings.Index(itemString, "}")
-		//fmt.Printf("i = %d, i3 = %d\n", i, i3)
-		NewJson = NewJson + "\"image_url\":"
-		NewJson = NewJson + " \"" + itemString[15:i3-1] //contains the url
-		if strings.Contains(itemString[16:i3-1], "\"") {
-			NewJson = NewJson + ","
-		} else {
+			NewJson = NewJson + "{\"image_url\":"
+			NewJson = NewJson + itemString[19:i3-1] //contains the url
 			NewJson = NewJson + "\","
-		}
 
-		//fmt.Printf("made it this far5")
+			//fmt.Printf("made it this far4")
 
-		i = strings.Index(itemString, "averageRating")
-
-		i2 = len(itemString) - 1
-		itemString = itemString[i:i2]
-		i3 = strings.Index(itemString, ",")
-		//fmt.Printf("i = %d, i3 = %d\n", i, i3)
-		temp := itemString[15:i3]
-		tempi := strings.Index(temp, "nul")
-		if tempi != -1 {
-			NewJson = NewJson + "\"rating\":"
-			NewJson = NewJson + " \"" + "" //contains the rating
-			NewJson = NewJson + "\","
-		} else {
-			NewJson = NewJson + "\"rating\":"
-			NewJson = NewJson + " \"" + itemString[15:i3] //contains the rating
-			NewJson = NewJson + "\","
-		}
-		//fmt.Printf("made it this far6")
-
-		i = strings.Index(itemString, "$")
-
-		i2 = len(itemString) - 1
-		itemString = itemString[i:i2]
-		i3 = strings.Index(itemString, ",")
-		//fmt.Printf("i = %d, i3 = %d\n", i, i3)
-		if strings.Contains(itemString[1:i3-1], "fals") {
-			NewJson = NewJson + "\"price\":" + "\"fals\","
-		} else {
-			NewJson = NewJson + "\"price\":"
-			NewJson = NewJson + "\"" + "$" + itemString[1:i3-1] //contains the price
-			if strings.Contains(itemString[1:i3-1], "\"") {
+			i = strings.Index(itemString, "\"title\"")
+			i2 = len(itemString) - 1
+			itemString = itemString[i:i2]
+			i3 = strings.Index(itemString, "}")
+			//fmt.Printf("i = %d, i3 = %d\n", i, i3)
+			NewJson = NewJson + "\"product_name\""
+			NewJson = NewJson + itemString[7:i3] //contains the name
+			if strings.Contains(itemString[8:i3], "\"") {
 				NewJson = NewJson + ","
 			} else {
 				NewJson = NewJson + "\","
 			}
-		}
-		NewJson = NewJson + "\"seller_name\":\"Walmart\"},"
 
-		//fmt.Printf("made it this far7")
-		//fmt.Printf(NewJson)
-		count++
+			//fmt.Printf("made it this far5")
+
+			i = strings.Index(itemString, "formatted_current_price\"")
+			i2 = len(itemString) - 1
+			itemString = itemString[i:i2]
+			i3 = strings.Index(itemString, ",")
+			//fmt.Printf("i = %d, i3 = %d\n", i, i3)
+			NewJson = NewJson + "\"price\":"
+			NewJson = NewJson + "\"" + itemString[26:i3] //contains the price
+			if strings.Contains(itemString[27:i3], "\"") {
+				if NewJson[len(NewJson)-1:] != "\"" {
+					NewJson = NewJson + "\""
+				}
+				NewJson = NewJson + ","
+			} else {
+				NewJson = NewJson + "\","
+			}
+			//fmt.Printf("made it this far6")
+
+			i = strings.Index(itemString, "average")
+			i2 = len(itemString) - 1
+			itemString = itemString[i:i2]
+			i3 = strings.Index(itemString, ",")
+			temp := itemString[9 : i3-1]
+			tempi := strings.Index(temp, "nul")
+			if tempi != -1 {
+				NewJson = NewJson + "\"rating\":"
+				NewJson = NewJson + "\"" + "" + "\"" //contains the rating
+				NewJson = NewJson + ","
+			} else {
+				//fmt.Printf("i = %d, i3 = %d\n", i, i3)
+				NewJson = NewJson + "\"rating\":"
+				NewJson = NewJson + "\"" + itemString[9:i3-1] + "\"" //contains the rating
+				NewJson = NewJson + ","
+			}
+			NewJson = NewJson + "\"seller_name\":\"Target\"},"
+
+			//fmt.Printf("made it this far7")
+			//fmt.Printf(NewJson)
+			count++
+		}
+		NewJson = NewJson[0 : len(NewJson)-2]
+		NewJson = NewJson + "},"
+
+		search, err = io.ReadAll(r.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//append = bytes.NewBuffer(search).String()
+
+		url = "https://walmart.p.rapidapi.com/products/v2/list?cat_id=0&sort=best_seller&page=1&query="
+
+		url = url + append
+		req, _ = http.NewRequest("GET", url, nil)
+
+		req.Header.Add("X-RapidAPI-Key", "813328aa2cmshbaf8f8dc041bb3ep1a7203jsnc647b6bcd1c7")
+		req.Header.Add("X-RapidAPI-Host", "walmart.p.rapidapi.com")
+
+		res, err = http.DefaultClient.Do(req)
+
+		if err != nil {
+			fmt.Println("An error connecting to Walmart occurred.")
+			return
+		}
+
+		defer res.Body.Close()
+		body, _ = io.ReadAll(res.Body)
+		i = 0
+		itemString = string(body)
+		i2 = len(itemString) - 1
+		i3 = 0
+		count = 0
+		for i != -1 {
+			if count >= 20 {
+				break
+			}
+			i = strings.Index(itemString, "\"Product\"")
+			if i == -1 {
+				break
+			}
+			i = strings.Index(itemString, "\"name\"")
+			if i == -1 {
+				break
+			}
+			i = strings.Index(itemString, "thumbnailUrl")
+			if i == -1 {
+				break
+			}
+			i = strings.Index(itemString, "averageRating")
+			if i == -1 {
+				break
+			}
+			i = strings.Index(itemString, "$")
+			if i == -1 {
+				break
+			}
+
+			i = strings.Index(itemString, "\"Product\"")
+
+			i2 = len(itemString) - 1
+			itemString = itemString[i:i2]
+			//fmt.Printf("made it this far3")
+
+			i = strings.Index(itemString, "\"name\"")
+
+			//fmt.Printf("made it this far4")
+			i2 = len(itemString) - 1
+			//fmt.Printf("i = %d, i2 = %d\n", i, i2)
+			itemString = itemString[i:i2]
+			//fmt.Printf("made it this far5")
+			i3 = strings.Index(itemString, ",")
+			//fmt.Printf("made it this far6")
+			//fmt.Printf("i = %d, i3 = %d\n", i, i3)
+			NewJson = NewJson + "{\"product_name\":"
+			NewJson = NewJson + itemString[7:i3-1] //contains the name
+			NewJson = NewJson + "\","
+
+			//fmt.Printf("made it this far4")
+
+			i = strings.Index(itemString, "thumbnailUrl")
+
+			i2 = len(itemString) - 1
+			itemString = itemString[i:i2]
+			i3 = strings.Index(itemString, "}")
+			//fmt.Printf("i = %d, i3 = %d\n", i, i3)
+			NewJson = NewJson + "\"image_url\":"
+			NewJson = NewJson + " \"" + itemString[15:i3-1] //contains the url
+			if strings.Contains(itemString[16:i3-1], "\"") {
+				NewJson = NewJson + ","
+			} else {
+				NewJson = NewJson + "\","
+			}
+
+			//fmt.Printf("made it this far5")
+
+			i = strings.Index(itemString, "averageRating")
+
+			i2 = len(itemString) - 1
+			itemString = itemString[i:i2]
+			i3 = strings.Index(itemString, ",")
+			//fmt.Printf("i = %d, i3 = %d\n", i, i3)
+			temp := itemString[15:i3]
+			tempi := strings.Index(temp, "nul")
+			if tempi != -1 {
+				NewJson = NewJson + "\"rating\":"
+				NewJson = NewJson + " \"" + "" //contains the rating
+				NewJson = NewJson + "\","
+			} else {
+				NewJson = NewJson + "\"rating\":"
+				NewJson = NewJson + " \"" + itemString[15:i3] //contains the rating
+				NewJson = NewJson + "\","
+			}
+			//fmt.Printf("made it this far6")
+
+			i = strings.Index(itemString, "$")
+
+			i2 = len(itemString) - 1
+			itemString = itemString[i:i2]
+			i3 = strings.Index(itemString, ",")
+			//fmt.Printf("i = %d, i3 = %d\n", i, i3)
+			if strings.Contains(itemString[1:i3-1], "fals") {
+				NewJson = NewJson + "\"price\":" + "\"fals\","
+			} else {
+				NewJson = NewJson + "\"price\":"
+				NewJson = NewJson + "\"" + "$" + itemString[1:i3-1] //contains the price
+				if strings.Contains(itemString[1:i3-1], "\"") {
+					NewJson = NewJson + ","
+				} else {
+					NewJson = NewJson + "\","
+				}
+			}
+			NewJson = NewJson + "\"seller_name\":\"Walmart\"},"
+
+			//fmt.Printf("made it this far7")
+			//fmt.Printf(NewJson)
+			count++
+		}
+		NewJson = NewJson[0 : len(NewJson)-2]
+		NewJson = NewJson + "}]"
+		//fmt.Print(NewJson)
 	}
-	NewJson = NewJson[0 : len(NewJson)-2]
-	NewJson = NewJson + "}]"
-	//fmt.Print(NewJson)
 
 	/*
 		for i != -1 {
